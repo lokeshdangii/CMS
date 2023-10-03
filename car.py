19be5efabb24ef81ea6eed9e2e22a8342ac3850c
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-03 12:38:40 +0530

    CRUD logic for car table

--- car.py
+++ car.py
@@ -1,37 +1,42 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
 import mysql.connector
+from db import db,cursor
 
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
-
-# Database configuration
-db_config = {
-    "host": "localhost",
-    "user": "root",
-    "password": "root",
-    "database": "cardb"
-}
-
-# Create a connection to the MySQL database
-db = mysql.connector.connect(**db_config)
-
-# Initialize a cursor
-cursor = db.cursor()
+car = Blueprint('car',__name__)
 
 # ------------------------------------- Display (select query) Car ---------------------------------------------------
 
-# Route to display all Cars
-@app.route('/car')
+# Route to display all Cars with related information
+@car.route('/car')
 def car_table():
-    cursor.execute("SELECT * FROM Car")
+    cursor.execute("""
+        SELECT 
+            C.CarID, 
+            CV.VariantName, 
+            CC.ColorName, 
+            CAT.CategoryName, 
+            CE.EngineName, 
+            CM.ModelName, 
+            C.VIN, 
+            C.Mileage, 
+            C.YearOfManufacture, 
+            C.BrandCompany 
+        FROM Car AS C
+        JOIN CarVariant AS CV ON C.VariantID = CV.VariantID
+        JOIN CarColor AS CC ON C.ColorID = CC.ColorID
+        JOIN CarCategory AS CAT ON C.CategoryID = CAT.CategoryID
+        JOIN CarEngine AS CE ON C.EngineID = CE.EngineID
+        JOIN CarModel AS CM ON C.ModelID = CM.ModelID
+    """)
     data = cursor.fetchall()
     return render_template('view/car.html', data=data)
+
 
 
 # ------------------------------------ Add/Insert Car ---------------------------------------------------
 
 # Route to add a new Car
-@app.route('/car/add', methods=['GET', 'POST'])
+@car.route('/car/add', methods=['GET', 'POST'])
 def add_car():
     if request.method == 'POST':
         variant_id = request.form['variant_id']
@@ -72,58 +77,42 @@ def add_car():
 # ------------------------------------ Update/Edit Car ---------------------------------------------------
 
 # Route to edit a Car
-@app.route('/car/edit', methods=['GET', 'POST'])
+@car.route('/car/edit', methods=['GET', 'POST'])
 def edit_car():
     if request.method == 'POST':
-        car_id = request.form['car_to_edit']
-        new_variant_id = request.form['new_variant_id']
-        new_category_id = request.form['new_category_id']
-        new_engine_id = request.form['new_engine_id']
-        new_color_id = request.form['new_color_id']
-        new_model_id = request.form['new_model_id']
-        new_vin = request.form['new_vin']
-        new_mileage = request.form['new_mileage']
-        new_year_of_manufacture = request.form['new_year_of_manufacture']
-        new_brand_company = request.form['new_brand_company']
-        
+        car_id = request.form['car_id']
+        field_to_update = request.form['field_to_update']
+        updated_value = request.form['updated_value']
+
         try:
-            update_query = "UPDATE Car SET VariantID = %s, CategoryID = %s, EngineID = %s, ColorID = %s, ModelID = %s, VIN = %s, Mileage = %s, YearOfManufacture = %s, BrandCompany = %s WHERE CarID = %s"
-            cursor.execute(update_query, (new_variant_id, new_category_id, new_engine_id, new_color_id, new_model_id, new_vin, new_mileage, new_year_of_manufacture, new_brand_company, car_id))
+            # update query on the selected field
+            update_query = f"UPDATE Car SET {field_to_update} = %s WHERE CarID = %s"
+            cursor.execute(update_query, (updated_value, car_id))
             db.commit()
-            flash('Car updated successfully', 'success')
-            return redirect('/car')  # Redirect to the Car list after editing
+            flash(f'Car {field_to_update} updated successfully', 'success')
+            return redirect('/car/edit')  
         except mysql.connector.Error as e:
             db.rollback()
             flash(f'Error updating Car: {e}', 'danger')
 
-    # Fetch the list of variants, categories, engines, colors, and models to populate dropdowns in the form
-    cursor.execute("SELECT CarID, VariantID, CategoryID, EngineID, ColorID, ModelID, VIN, Mileage, YearOfManufacture, BrandCompany FROM Car")
+    # Fetch the list of cars to populate the dropdown in the form
+    cursor.execute("SELECT CarID, VIN FROM Car")
     cars = cursor.fetchall()
-    cursor.execute("SELECT VariantID, VariantName FROM CarVariant")
-    variants = cursor.fetchall()
-    cursor.execute("SELECT CategoryID, CategoryName FROM CarCategory")
-    categories = cursor.fetchall()
-    cursor.execute("SELECT EngineID, EngineName FROM CarEngine")
-    engines = cursor.fetchall()
-    cursor.execute("SELECT ColorID, ColorName FROM CarColor")
-    colors = cursor.fetchall()
-    cursor.execute("SELECT ModelID, ModelName FROM CarModel")
-    models = cursor.fetchall()
 
-    return render_template('update/edit_car.html', cars=cars, variants=variants, categories=categories, engines=engines, colors=colors, models=models)
+    return render_template('update/edit_car.html', cars=cars)
 
 
 # --------------------------- Delete Car ---------------------------------------------------
 
 # Route to display the Car deletion form
-@app.route('/car/delete', methods=['GET'])
+@car.route('/car/delete', methods=['GET'])
 def delete_car_form():
-    cursor.execute("SELECT CarID, VIN FROM Car")
+    cursor.execute("SELECT CarID, VIN, BrandCompany FROM Car")
     cars = cursor.fetchall()
     return render_template('delete/delete_car.html', cars=cars)
 
 # Route for deleting a Car
-@app.route('/car/delete', methods=['POST'])
+@car.route('/car/delete', methods=['POST'])
 def delete_car():
     if request.method == 'POST':
         car_id = request.form.get('car_to_delete')
@@ -136,9 +125,5 @@ def delete_car():
             db.rollback()
             flash(f'Error deleting Car: {e}', 'danger')
 
-        return redirect('/car')
+        return redirect('/car/delete')
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
