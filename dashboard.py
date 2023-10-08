087e2c20debc0fb9ba96117b98610c9c06018728
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-09 03:15:55 +0530

    dashboard of CSMS

--- dashboard.py
+++ dashboard.py
@@ -2,20 +2,32 @@ from flask import Blueprint, render_template
 
 dashboard = Blueprint('dashboard', __name__)
 
+# dashboard for car
 @dashboard.route('/car_dashboard')
 def car_dashboard():
     return render_template('car.html')
 
+# dashboard for carmodel
 @dashboard.route('/carmodel_dashboard')
 def carmodel_dashboard():
     return render_template('car_model.html')
 
+# dashboard for carcolor
 @dashboard.route('/carcolor_dashboard')
 def carcolor_dashboard():
     return render_template('car_color.html')
 
+# dashboard for car category
 @dashboard.route('/carcategory_dashboard')
 def carcategory_dashboard():
     return render_template('car_category.html')
 
-# Add more routes for other sections as needed
+# dashboard for car engine
+@dashboard.route('/carengine_dashboard')
+def carengine_dashboard():
+    return render_template('car_engine.html')
+
+# dashboard for car variant
+@dashboard.route('/carvariant_dashboard')
+def carvariant_dashboard():
+    return render_template('car_variant.html')
