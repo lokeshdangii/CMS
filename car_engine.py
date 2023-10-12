8a33a75ce61d01cc55b18a0231ce514329c700d8
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-13 03:03:28 +0530

    file to manage car engine CRUD

--- car_engine.py
+++ car_engine.py
@@ -2,14 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for, flash, Blu
 from db import db,cursor
 import mysql.connector
 
-car_engine = Blueprint('car_engine',__name__)
+manage_car_engine = Blueprint('manage_car_engine',__name__)
 
 # ------------------------------------- Display (select query) Car Engine ---------------------------------------------------
 
 # Route to display all Car Engines
-@car_engine.route('/carengine')
+@manage_car_engine.route('/manage_car_engine')
 def carengine_table():
-    cursor.execute("SELECT * FROM CarEngine")
+    cursor.execute("SELECT EngineID,EngineName FROM CarEngine")
     data = cursor.fetchall()
     return render_template('view/carEngine.html', data=data)
 
@@ -17,7 +17,7 @@ def carengine_table():
 # ------------------------------------ Add/Insert Car Engine ---------------------------------------------------
 
 # Route to add a new Car Engine
-@car_engine.route('/carengine/add', methods=['GET', 'POST'])
+@manage_car_engine.route('/manage_car_engine/add', methods=['GET', 'POST'])
 def add_carengine():
     if request.method == 'POST':
         engine_id = request.form['engine_id']
@@ -39,45 +39,51 @@ def add_carengine():
 
 # ------------------------------------ Update/Edit Car Engine ---------------------------------------------------
 # Route to edit a Car Engine
-@car_engine.route('/carengine/edit', methods=['GET', 'POST'])
-def edit_car_engine():
+@manage_car_engine.route('/manage_car_engine/edit/<int:engine_id>', methods=['GET', 'POST'])
+def edit_car_engine(engine_id):
     if request.method == 'POST':
-        engine_id = request.form['engine_to_edit']
+        
         new_engine_name = request.form['new_engine_name']
 
-        update_query = "UPDATE CarEngine SET EngineName = %s WHERE EngineID = %s"
-        cursor.execute(update_query, (new_engine_name, engine_id))
-        db.commit()
-        return render_template('success.html')  # Redirect to the Car Engine list after editing
-    else:
-        cursor.execute("SELECT * FROM CarEngine")
-        engines = cursor.fetchall()
-        return render_template('update/edit_carengine.html', engines=engines)
+        try:
+            update_query = "UPDATE CarEngine SET EngineName = %s WHERE EngineID = %s"
+            cursor.execute(update_query, (new_engine_name,engine_id))
+            db.commit()
+            flash('Car Engine updated successfully', 'success')
+            return render_template('success.html')
+        
+        except mysql.connector.Error as e:
+            db.rollback()
+            flash(f'Error updating Car Engine : {e}', 'danger')
+        
+    # fetch car color to edit
+    fetch_query = "Select EngineID,EngineName from CarEngine where EngineID = %s"
+    cursor.execute(fetch_query,(engine_id,))
+    engine_data = cursor.fetchone()
+
+    if engine_data is None:
+        flash('Car Engine not found','danger')
+        return redirect(url_for('manage_car_engine.carengine_table'))
+    
+    return render_template('update/edit_carengine.html', engine_data = engine_data)
+
 
 
 # --------------------------- delete Car Engine ---------------------------------------------------
 
-# Route to display the Car Engine deletion form
-@car_engine.route('/carengine/delete', methods=['GET'])
-def delete_car_engine_form():
-    cursor.execute("SELECT EngineID, EngineName FROM CarEngine")
-    engines = cursor.fetchall()
-    return render_template('delete/delete_carengine.html', engines=engines)
-
 # Route for deleting a Car Engine
-@car_engine.route('/carengine/delete', methods=['POST'])
-def delete_car_engine():
-    if request.method == 'POST':
-        engine_id = request.form.get('engine_to_delete')
+@manage_car_engine.route('/manage_car_engine/delete/<int:engine_id>', methods=['GET', 'POST'])
+def delete_car_engine(engine_id):
+    
+    try:
+        delete_query = "DELETE from CarEngine where EngineID = %s"
+        cursor.execute(delete_query,(engine_id,))
+        db.commit()
+        flash(f"Car Engine with EngineID: { engine_id} deleted successfully", 'success')
+        return render_template('success.html')
 
-        # Perform the deletion
-        try:
-            cursor.execute("DELETE FROM CarEngine WHERE EngineID = %s", (engine_id,))
-            db.commit()
-            return render_template('success.html')
-        except mysql.connector.Error as err:
-            db.rollback()
-            return f"Error deleting Car Engine: {err}"
+    except mysql.connector.Error as e:
+        db.rollback()
+        flash(f'Error deleting Car Engine: {e}', 'danger')
 
-        # Redirect back to the Car Engine deletion form
-        return redirect('/carengine/delete')
+    return redirect(url_for('manage_car_engine.carengine_table'))
