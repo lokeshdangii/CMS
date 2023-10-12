54981c7bb3c1a1e06acb0280de3a285db32c0193
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-13 03:05:20 +0530

    file to redirect to dashboard

--- dashboard.py
+++ dashboard.py
@@ -5,29 +5,29 @@ dashboard = Blueprint('dashboard', __name__)
 # dashboard for car
 @dashboard.route('/car_dashboard')
 def car_dashboard():
-    return render_template('car.html')
+    return render_template('dashboard/car.html')
 
 # dashboard for carmodel
 @dashboard.route('/carmodel_dashboard')
 def carmodel_dashboard():
-    return render_template('car_model.html')
+    return render_template('dashboard/car_model.html')
 
 # dashboard for carcolor
 @dashboard.route('/carcolor_dashboard')
 def carcolor_dashboard():
-    return render_template('car_color.html')
+    return render_template('dashboard/car_color.html')
 
 # dashboard for car category
 @dashboard.route('/carcategory_dashboard')
 def carcategory_dashboard():
-    return render_template('car_category.html')
+    return render_template('dashboard/car_category.html')
 
 # dashboard for car engine
 @dashboard.route('/carengine_dashboard')
 def carengine_dashboard():
-    return render_template('car_engine.html')
+    return render_template('dashboard/car_engine.html')
 
 # dashboard for car variant
 @dashboard.route('/carvariant_dashboard')
 def carvariant_dashboard():
-    return render_template('car_variant.html')
+    return render_template('dashboard/car_variant.html')
