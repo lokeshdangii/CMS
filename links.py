09dba2f744ddb06250f4e4032d4a38185c566173
lokeshdangii <lokeshdangi1045@gmail.com>
2023-10-21 00:49:44 +0530

    redirection links of different tables

--- links.py
+++ links.py
@@ -61,3 +61,17 @@ def category_table(category_id):
     count = cursor.fetchone()
     return render_template('links/category.html', cars = cars, count = count)
 
+
+# ------------------------------------- MODEL  -----------------------------------------------------------------
+
+# Route to display all Cars of Specific Category
+@links.route('/model_link/<int:model_id>')
+@login_required
+def model_table(model_id):
+    cursor.execute("SELECT CarModel.ModelName, CarColor.ColorName, CarVariant.VariantName FROM Car INNER JOIN CarModel ON Car.ModelID = CarModel.ModelID INNER JOIN CarColor ON Car.ColorID = CarColor.ColorID inner join CarVariant on Car.VariantID = CarVariant.VariantID WHERE CarModel.ModelID =  %s", (model_id,))
+    cars = cursor.fetchall()
+
+    cursor.execute("select COUNT(*) ModelName, ModelName from CarModel inner join Car  on Car.ModelID = CarModel.ModelID  where Car.ModelID = %s", (model_id,))
+    count = cursor.fetchone()
+    return render_template('links/model.html', cars = cars, count = count)
+
