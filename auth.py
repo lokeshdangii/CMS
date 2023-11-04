916c5ad9e2f49bad9d359ed5d4a45c3a87857a84
lokeshdangii <lokeshdangi1045@gmail.com>
2023-11-04 17:05:59 +0530

    updated auth file

--- auth.py
+++ auth.py
@@ -39,24 +39,24 @@ def register():
         hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
 
         # Insert the user into the database
-        insert_user_query = "INSERT INTO User (Username, Password) VALUES (%s, %s)"
+        insert_user_query = "INSERT INTO User (Username, Password, role) VALUES (%s, %s, 'User')"
         user_data = (username, hashed_password)
 
         try:
             cursor.execute(insert_user_query, user_data)
             db.commit()
             flash('Registration successful! You can now login.', 'success')
-            return redirect(url_for('auth.login_form'))
+            return redirect(url_for('auth.user_login_form'))
         except mysql.connector.Error as e:
             db.rollback()
             flash(f'Registration failed: {e}', 'danger')
             
 
-# ------------------------------------------------Login Code -----------------------------------------
+# ------------------------------------------------Admin Login Code -----------------------------------------
         
 @auth.route('/login')
 def login_form():
-    return render_template('auth/login.html')
+    return render_template('auth/adminlogin.html')
 
 @auth.route('/login', methods=['POST'])
 def login():
@@ -65,19 +65,49 @@ def login():
         password = request.form['password']
 
         # Query the database to fetch the user's hashed password
-        get_user_query = "SELECT Username, Password FROM User WHERE Username = %s"
+        get_user_query = "SELECT Username, Password, role FROM Admin WHERE Username = %s"
         cursor.execute(get_user_query, (username,))
         user_data = cursor.fetchone()
 
-        if user_data and check_password_hash(user_data[1], password):
+        if user_data and password:
             # Successful login, store user data in session
-            session['username'] = user_data[0]
+            session['username'] = user_data[0] # Store the username in the session
+            session['role'] = user_data[2]  # Store the role in the session
             flash('Login successful!', 'success')
             return redirect(url_for('auth.dashboard'))
         else:
             flash('Invalid username or password', 'danger')
 
-    return render_template('auth/login.html')
+    return render_template('auth/adminlogin.html')
+
+
+# -------------------------------------------- User Login Code ----------------------------------------
+
+@auth.route('/userlogin')
+def user_login_form():
+    return render_template('auth/userlogin.html')
+
+@auth.route('/userlogin', methods=['POST'])
+def user_login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        # Query the database to fetch the user's hashed password
+        get_user_query = "SELECT Username, Password, role FROM User WHERE Username = %s"
+        cursor.execute(get_user_query, (username,))
+        user_data = cursor.fetchone()
+
+        if user_data and check_password_hash(user_data[1], password):
+            # Successful login, store user data in session
+            session['username'] = user_data[0]  # Store the username in the session
+            session['role'] = user_data[2]    # Store the role in the session
+            flash('Login successful!', 'success')
+            return redirect(url_for('auth.dashboard'))
+        else:
+            flash('Invalid username or password', 'danger')
+
+    return render_template('auth/userlogin.html')
 
 
 
@@ -106,8 +136,6 @@ def login_required(view):
             return redirect(url_for('auth.login_form'))
 
     return wrapped_view
-
-
 
 
 
